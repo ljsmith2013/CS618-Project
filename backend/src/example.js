d490ebcb4e0af09f3565a95eeecc4470f0a51170
ljsmith2013 <160754350+ljsmith2013@users.noreply.github.com>
2025-12-11 15:49:00 -0500

    Add files via upload
    Initial Project Setup

---
+++ backend/src/example.js
@@ -0,0 +1,19 @@
+import { initDatabase } from "./db/init.js";
+
+import { Post } from "./db/models/post.js";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+await initDatabase();
+
+const post = new Post({
+  title: "Hello second post!",
+  author: "John Doe",
+  contents: "New exciting content",
+  tags: ["frontend"],
+});
+await post.save();
+
+const posts = await Post.find();
+console.log(posts);
